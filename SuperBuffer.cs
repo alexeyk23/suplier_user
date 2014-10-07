0c90ea2c7292234ab5fbd63cb5574ef923fec2d1
trebushet <kunak94@mail.ru>
2014-10-07 20:36:13 +0400

    add 2 semaphore

--- SuperBuffer.cs
+++ SuperBuffer.cs
@@ -9,7 +9,8 @@ namespace supplier_user
     class SuperBuffer
     {
         Queue<int> buf = new Queue<int>();
-        Semaphore semReader;
+        Semaphore semFull;
+        Semaphore semEmpty;
         int countReader;//количество читателей
         int sizeBuffer;
         Mutex mut;
@@ -18,7 +19,8 @@ namespace supplier_user
         {
             this.countReader = countReader;
             this.sizeBuffer = sizeBuffer;
-            semReader = new Semaphore(countReader, countReader);
+            semEmpty = new Semaphore(sizeBuffer, sizeBuffer);
+            semFull = new Semaphore(0, sizeBuffer);
             mut = new Mutex();
             this.dgv = dgv;
         }
@@ -27,22 +29,22 @@ namespace supplier_user
         {
             object[] values = new object[3];
            
-            semReader.WaitOne();//ждать семафора
+            semEmpty.WaitOne();//ждать семафора
             mut.WaitOne();//войти в критическую секцию
-            values[0] = "";
-            StringBuilder sb = new StringBuilder();            
-            if (buf.Count != 0)                
-                values[2] =Thread.CurrentThread.Name+" взял "+ buf.Dequeue();                
-            else                
-                values[2] = Thread.CurrentThread.Name+" не смог взять";
-            foreach (int item in buf)
-            {
-                sb.Append(item + " ");
-            }
-            values[1] = sb.ToString();
-            dgv.Rows.Add(values);            
+                values[0] = "";
+                StringBuilder sb = new StringBuilder();            
+                if (buf.Count != 0)                
+                    values[2] =Thread.CurrentThread.Name+" взял "+ buf.Dequeue();                
+                else                
+                    values[2] = Thread.CurrentThread.Name+" не смог взять";
+                foreach (int item in buf)
+                {
+                    sb.Append(item + " ");
+                }
+                values[1] = sb.ToString();
+                dgv.Rows.Add(values);            
             mut.ReleaseMutex();
-            semReader.Release();            
+            semFull.Release();            
 
         }
         //попробуй положить (для поставщика)
@@ -50,34 +52,36 @@ namespace supplier_user
         {
             
             object[] values = new object[3];
+            semFull.WaitOne();
             mut.WaitOne();
-            values[2] = "";            
-            if (buf.Count != sizeBuffer)
-            {
-                Random r = new Random();                   
-                //поставщик кладет случайное колмичество 
-                int n = r.Next(sizeBuffer - buf.Count);
-                n = n == 0 ? 1 : n;
-                values[0] = "Писатель положил ";
-                while (n>0)
+                values[2] = "";            
+                if (buf.Count != sizeBuffer)
                 {
-                    int a = r.Next(100);
-                    buf.Enqueue(a);
-                    n--;
-                    values[0]+= a.ToString()+" ";
-                }
+                    Random r = new Random();                   
+                    //поставщик кладет случайное колмичество 
+                    int n = r.Next(sizeBuffer - buf.Count);
+                    n = n == 0 ? 1 : n;
+                    values[0] = "Писатель положил ";
+                    while (n>0)
+                    {
+                        int a = r.Next(100);
+                        buf.Enqueue(a);
+                        n--;
+                        values[0]+= a.ToString()+" ";
+                    }
                
-            }
-            else            
-                values[0] = "Писатель ничего не положил";
-            StringBuilder sb = new StringBuilder();
-            foreach (int item in buf)
-            {
-                sb.Append(item + " ");
-            }
-            values[1] = sb.ToString();
-            dgv.Rows.Add(values);          
+                }
+                else            
+                    values[0] = "Писатель ничего не положил";
+                StringBuilder sb = new StringBuilder();
+                foreach (int item in buf)
+                {
+                    sb.Append(item + " ");
+                }
+                values[1] = sb.ToString();
+                dgv.Rows.Add(values);          
             mut.ReleaseMutex();
+            semEmpty.Release();
         }
              
     }
